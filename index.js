188cc602adaac06c4545f67fdf9688c8c852fb60
Andrew <andrewonulak@gmail.com>
2022-10-13 14:02:34 -0600

    commit tree c8406bd0f9064c2e0898f2cf690e43e630d5169d

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = 2000
+let b = 100
+
+let divide = function(a, b) {
+    return (a / b)
+}
+divide()
+
+const square = x => x * x;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
